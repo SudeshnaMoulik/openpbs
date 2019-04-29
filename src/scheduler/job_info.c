667789cee922a1637f83ff23880a9aeba16dd890
nehap <nehaisat10@gmail.com>
2019-04-30 01:17:26 +0530

    math module: fix job sort formula errors

--- src/scheduler/job_info.c
+++ src/scheduler/job_info.c
@@ -4140,7 +4140,7 @@ formula_evaluate(char *formula, resource_resv *resresv, resource_req *resreq)
 		"ex = None\n"
 		"try:\n"
 		"\t_FORMANS_ = eval(\"%s\", globals_dict, locals())\n"
-		"except Exception, ex:"
+		"except Exception as ex:"
 		"\t_PBS_PYTHON_EXCEPTIONSTR_=str(ex)\n", formula);
 
 	PyRun_SimpleString(formula_buf);
